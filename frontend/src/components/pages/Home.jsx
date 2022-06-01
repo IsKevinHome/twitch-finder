6123fcd7ea8a45c68effcf49315d883e4f279973
Kevin Muhlbach <HeadbuttStudios@gmail.com>
2022-05-31 22:45:28 -0700

    changed font found image

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -2,7 +2,7 @@ import Typography from "@mui/material/Typography";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import ChannelSearch from "../channels/ChannelSearch";
-import searchImage from "../../assets/photos/blueSearch.jpg";
+import gameControl from "../../assets/photos/clipart.png";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 
@@ -28,45 +28,24 @@ const Home = () => {
                                 variant="h5"
                                 component="h1"
                                 gutterBottom
+                                style={{ fontFamily: "dimis" }}
                             >
-                                SEARCH
+                                Search Twitch Channels
                             </Typography>
 
                             <Typography variant="body1" gutterBottom>
-                                body1. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Quos blanditiis tenetur unde
-                                suscipit, quam beatae rerum inventore
-                                consectetur, neque doloribus, cupiditate numquam
-                                dignissimos laborum fugiat deleniti? Eum quasi
-                                quidem quibusdam.
+                                Welcome to Twitch Finder! You can search for any
+                                channels to see who is live and some other
+                                information.
                             </Typography>
                         </Paper>
                         <ChannelSearch />
                     </Grid>
 
                     <Grid item xs={12} md={6} align="center">
-                        <Paper
-                            elevation={0}
-                            sx={{ maxWidth: 500, textAlign: "left" }}
-                        >
-                            <Typography
-                                variant="h5"
-                                component="h1"
-                                gutterBottom
-                            >
-                                SEARCH
-                            </Typography>
-
-                            <Typography variant="body1" gutterBottom>
-                                body1. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Quos blanditiis tenetur unde
-                                suscipit, quam beatae rerum inventore
-                                consectetur, neque doloribus, cupiditate numquam
-                                dignissimos laborum fugiat deleniti? Eum quasi
-                                quidem quibusdam.
-                            </Typography>
+                        <Paper elevation={0} sx={{ maxWidth: 500 }}>
+                            <img src={gameControl} style={{ width: "80%" }} />
                         </Paper>
-                        <ChannelSearch />
                     </Grid>
                 </Grid>
             </Box>
